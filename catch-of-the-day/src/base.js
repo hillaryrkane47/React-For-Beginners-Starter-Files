a7923ed83cb5b29249a81776329ca0267bc61b91
Hillary Kane <hillaryrkane47@gmail.com>
2016-12-29 11:07:33 -0500

    added content for video 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCZx7MzXap7lXXkvOD_hGBVeJWthUveZbI",
+  authDomain: "catch-of-the-day-hillary-kane.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-hillary-kane.firebaseio.com",
+});
+
+export default base;
--- catch-of-the-day/src/helpers.js
+++ catch-of-the-day/src/helpers.js
@@ -1,4 +1,4 @@
-export function formatPrice(cents) {
+ export function formatPrice(cents) {
   return `$${(cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
 }
 
